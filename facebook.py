daa6af882ad11764f81354c742efaac172d963bb
Firoms <edisonmessi@naver.com>
2021-01-13 20:45:33 +0900

    2021/01/13

--- facebook.py
+++ facebook.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-URL = 'https://ko-kr.facebook.com/'
+URL = 'https://naver.com/'
 cOptions = Options()
 cOptions.add_argument("--disable-infobars")
 cOptions.add_argument("--disable-extensions")
@@ -10,21 +10,30 @@ cOptions.add_experimental_option("prefs", {
 })
 
 
-id = ''
-pw = ''
+driver = webdriver.Chrome(chrome_options= cOptions, executable_path = 'C:\\코딩\\파이썬연습\\chromedriver.exe')
 
-driver = webdriver.Chrome(chrome_options= cOptions, executable_path = 'C:\\Users\\Coding Lab\\Downloads\\chromedriver_win32\\chromedriver.exe')
-
-driver.implicitly_wait(3)
+driver.implicitly_wait(1)
 driver.get(URL)
-driver.implicitly_wait(3)
+driver.implicitly_wait(1)
 
-driver.find_element_by_name('email').send_keys(id)
-driver.find_element_by_name('pass').send_keys(pw)
-driver.find_element_by_class_name('login_form_login_button').click()
-# driver.find_element_by_xpath('//*[@id="u_0_2"]').click() >>가끔 변함
-driver.find_element_by_name('xhpc_message').click()
-driver.implicitly_wait(3)
-article = '안녕하세요. 반갑습니다.'
-driver.find_element_by_class_name('_5yk2').send_keys(article)
-driver.find_element_by_class_name('_1mf7').click()
+
+
+
+
+
+
+
+
+
+# id = ''
+# pw = ''
+
+# driver.find_element_by_name('email').send_keys(id)
+# driver.find_element_by_name('pass').send_keys(pw)
+# driver.find_element_by_class_name('login_form_login_button').click()
+# # driver.find_element_by_xpath('//*[@id="u_0_2"]').click() >>가끔 변함
+# driver.find_element_by_name('xhpc_message').click()
+# driver.implicitly_wait(3)
+# article = '안녕하세요. 반갑습니다.'
+# driver.find_element_by_class_name('_5yk2').send_keys(article)
+# driver.find_element_by_class_name('_1mf7').click()
