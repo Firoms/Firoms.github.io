21138f175cd77b7d635f988fe6bb22753392e320
Firoms <edisonmessi@naver.com>
2021-06-04 21:50:03 +0900

    2021/06/04

--- Thread.py
+++ Thread.py
@@ -1,5 +1,10 @@
-import threading
+'''
+threading 모듈은 동시성을 이용할 수 있게 해주는 모듈이다.
+사실 동시라기보단 빠르게 번갈아 작업하는 형식인데,
+직접 이용해본 결과, threading은 예측 불가한다는 생각이 들었다.
+'''
 
+import threading
 
 def sum(low, high):
     total = 0
