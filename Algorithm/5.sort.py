cd2bc7bb33bad8758b3e1a11500d9daab855114e
Firoms <edisonmessi@naver.com>
2021-07-07 18:47:22 +0900

    2021/07/07

---
+++ Algorithm/5.sort.py
@@ -0,0 +1,19 @@
+# 정렬
+
+"""
+Input : nums = [2, 4, 5, 1, 3]
+
+Output : [1, 2, 3, 4, 5]
+"""
+
+
+class Solution:
+    def select_sort(self, nums: list):
+        return op
+
+
+if __name__ == "__main__":
+    nums = [2, 4, 5, 1, 3]
+
+    op1 = Solution().select_sort(nums)
+    print(op1)
