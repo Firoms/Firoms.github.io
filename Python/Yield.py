122d166b550c72202c7bdeb47095242258e0e0e3
Firoms <edisonmessi@naver.com>
2021-09-14 19:33:09 +0900

    2021/09/14

--- Python/Yield.py
+++ Python/Yield.py
@@ -2,10 +2,11 @@
 Yield
 Yield는 generator 생성자라 볼 수 있다.
 iterator와 비슷하다.
+range 자료형을 만든 원리로 볼 수 있을 것 같다.
 '''
 
 
-def gen_num():
+def genNum():
     print("first number")
     yield 1
     print("second number")
@@ -15,9 +16,9 @@ def gen_num():
 
 
 if __name__ == "__main__":    
-    gen_num()
-    print(type(gen_num()))
-    gen = gen_num()
+    genNum()
+    print(type(genNum()))
+    gen = genNum()
     next(gen)
     next(gen)
     next(gen)
